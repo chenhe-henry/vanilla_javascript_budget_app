31d58eaf90a3eb9247f362a18071c4f0a49213b2
chenhe-henry <chenhe.elec@gmail.com>
2020-04-16 22:02:30 +1000

    arrange todos in modules

--- app.js
+++ app.js
@@ -5,3 +5,17 @@
 // 4. Add the new item to the UI
 // 5. Calculate budget
 // 6. Update the UI
+
+//Use module to handle todo:
+
+// UI Module
+// 2. Get input values
+// 4. Add the new item to the UI
+// 6. Update the UI
+
+//Data Module
+// 3. Add the new item to our data structure
+// 5. Calculate budget
+
+//Controller Module
+// 1. Add event handler
