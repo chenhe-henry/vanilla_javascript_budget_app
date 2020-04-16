5fd137d3229a9e9675571d75a68703a155f55ad6
chenhe-henry <chenhe.elec@gmail.com>
2020-04-16 17:44:58 +1000

    add todos

---
+++ app.js
@@ -0,0 +1,7 @@
+// todo:
+// 1. Add event handler
+// 2. Get input values
+// 3. Add the new item to our data structure
+// 4. Add the new item to the UI
+// 5. Calculate budget
+// 6. Update the UI
